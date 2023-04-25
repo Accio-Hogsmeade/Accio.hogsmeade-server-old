5e80df13c2b4dbd70bd5d0be8444ed2fa8441920
Chaos0103 <lyt1228@naver.com>
2023-04-25 16:41:58 +0900

    🧙 feat: implement join member api controller

---
+++ src/main/java/accio/hogsmeade/store/member/controller/MemberApiController.java
@@ -0,0 +1,44 @@
+package accio.hogsmeade.store.member.controller;
+
+import accio.hogsmeade.store.member.controller.dto.JoinMemberRequest;
+import accio.hogsmeade.store.member.model.Authority;
+import accio.hogsmeade.store.member.model.Identity;
+import accio.hogsmeade.store.member.model.service.MemberService;
+import accio.hogsmeade.store.member.model.service.dto.AddMemberDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class MemberApiController {
+
+    public final MemberService memberService;
+
+    @PostMapping("/join")
+    public int join(@Valid @RequestBody JoinMemberRequest request) {
+        if (request.getIdentity() == Identity.MUGGLE && request.getAuthority() != Authority.MEMBER) {
+            return -1;
+        }
+
+        AddMemberDto addMemberDto = AddMemberDto.builder()
+                .loginId(request.getLoginId())
+                .loginPw(request.getLoginPw())
+                .username(request.getUsername())
+                .tel(request.getTel())
+                .mainAddress(request.getMainAddress())
+                .detailAddress(request.getDetailAddress())
+                .identity(request.getIdentity())
+                .authority(request.getAuthority())
+                .build();
+
+        Long memberId = memberService.joinMember(addMemberDto);
+        log.debug("memberId = {}", memberId);
+        return 1;
+    }
+}
