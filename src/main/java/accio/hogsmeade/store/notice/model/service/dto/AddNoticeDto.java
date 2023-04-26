b515adcfe928488e56a6bcc883f10fe1fda01605
chaos0103 <lyt1228@naver.com>
2023-04-26 20:40:03 +0900

    🚂 test: 공지사항 등록 테스트 작성

--- src/main/java/accio/hogsmeade/store/notice/model/service/dto/AddNoticeDto.java
+++ src/main/java/accio/hogsmeade/store/notice/model/service/dto/AddNoticeDto.java
@@ -1,5 +1,6 @@
 package accio.hogsmeade.store.notice.model.service.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
@@ -8,4 +9,11 @@ public class AddNoticeDto {
     private String title;
     private String content;
     private String pin;
+
+    @Builder
+    public AddNoticeDto(String title, String content, String pin) {
+        this.title = title;
+        this.content = content;
+        this.pin = pin;
+    }
 }
