e66477c4d39fe803bd0eeb8661db476ec69f4472
chaos0103 <lyt1228@naver.com>
2023-04-29 09:06:54 +0900

    🧙 feat: 알림 도메인 생성

---
+++ src/main/java/accio/hogsmeade/store/alarm/Alarm.java
@@ -0,0 +1,32 @@
+package accio.hogsmeade.store.alarm;
+
+import accio.hogsmeade.store.common.model.TimeBaseEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import static lombok.AccessLevel.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Alarm extends TimeBaseEntity {
+
+    @Id @GeneratedValue
+    @Column(name = "alarm_id")
+    private Long id;
+    private String content;
+    private Boolean open;
+
+    @Builder
+    public Alarm(Long id, String content, Boolean open) {
+        this.id = id;
+        this.content = content;
+        this.open = open;
+    }
+}
