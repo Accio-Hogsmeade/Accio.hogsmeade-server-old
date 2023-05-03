b1efe34799be53feca8efd5066b28699b3e14045
Chaos0103 <lyt1228@naver.com>
2023-05-03 15:09:29 +0900

    🔮 refactor: Alarm 컬럼 추가

--- src/main/java/accio/hogsmeade/store/alarm/model/Alarm.java
+++ src/main/java/accio/hogsmeade/store/alarm/model/Alarm.java
@@ -1,5 +1,6 @@
 package accio.hogsmeade.store.alarm.model;
 
+import accio.hogsmeade.store.common.model.Active;
 import accio.hogsmeade.store.common.model.TimeBaseEntity;
 import accio.hogsmeade.store.member.model.Member;
 import lombok.Builder;
@@ -24,17 +25,21 @@ public class Alarm extends TimeBaseEntity {
     private String content;
     @Column(nullable = false)
     private Boolean open;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Active active;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
     @Builder
-    public Alarm(Long id, String title, String content, Boolean open, Member member) {
+    public Alarm(Long id, String title, String content, Boolean open, Active active, Member member) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.open = open;
+        this.active = active;
         this.member = member;
     }
 
