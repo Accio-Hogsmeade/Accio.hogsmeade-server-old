c769ddc344a8a3f67749af4a536811c76de46abf
chaos0103 <lyt1228@naver.com>
2023-05-04 10:40:15 +0900

    🧙 feat: 매장 등록 서비스 구현

--- src/main/java/accio/hogsmeade/store/store/model/service/StoreService.java
+++ src/main/java/accio/hogsmeade/store/store/model/service/StoreService.java
@@ -1,7 +1,11 @@
 package accio.hogsmeade.store.store.model.service;
 
+import accio.hogsmeade.store.store.model.service.dto.AddStoreDto;
+
 import javax.transaction.Transactional;
 
 @Transactional
 public interface StoreService {
+
+    Long registerStore(String loginId, AddStoreDto dto);
 }
--- src/main/java/accio/hogsmeade/store/store/model/service/impl/StoreServiceImpl.java
+++ src/main/java/accio/hogsmeade/store/store/model/service/impl/StoreServiceImpl.java
@@ -1,10 +1,15 @@
 package accio.hogsmeade.store.store.model.service.impl;
 
+import accio.hogsmeade.store.member.model.Member;
 import accio.hogsmeade.store.member.model.repository.MemberRepository;
+import accio.hogsmeade.store.store.model.Store;
 import accio.hogsmeade.store.store.model.repository.StoreRepository;
 import accio.hogsmeade.store.store.model.service.StoreService;
+import accio.hogsmeade.store.store.model.service.dto.AddStoreDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +18,18 @@ public class StoreServiceImpl implements StoreService {
     private final StoreRepository storeRepository;
     private final MemberRepository memberRepository;
 
+    @Override
+    public Long registerStore(String loginId, AddStoreDto dto) {
+        Member findMember = memberRepository.findByLoginId(loginId)
+                .orElseThrow(NoSuchElementException::new);
 
+        Store store = Store.builder()
+                .storeName(dto.getStoreName())
+                .content(dto.getContent())
+                .uploadFile(dto.getUploadFile())
+                .build();
+
+        Store savedStore = storeRepository.save(store);
+        return savedStore.getId();
+    }
 }
