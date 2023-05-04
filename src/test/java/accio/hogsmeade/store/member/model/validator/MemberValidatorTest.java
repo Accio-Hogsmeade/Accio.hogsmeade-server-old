4badf4b60b55c31f0ad4a66a6fa9fb877f9055fe
chaos0103 <lyt1228@naver.com>
2023-05-04 13:48:23 +0900

    🚂 test: 회원 조회 검증기 테스트 작성

---
+++ src/test/java/accio/hogsmeade/store/member/model/validator/MemberValidatorTest.java
@@ -0,0 +1,74 @@
+package accio.hogsmeade.store.member.model.validator;
+
+import accio.hogsmeade.store.common.model.Address;
+import accio.hogsmeade.store.member.model.Member;
+import accio.hogsmeade.store.member.model.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+import java.util.NoSuchElementException;
+
+import static accio.hogsmeade.store.member.model.Grade.QUAFFLE;
+import static accio.hogsmeade.store.member.model.Identity.STUDENT;
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberValidatorTest {
+
+    @Autowired
+    private MemberValidator memberValidator;
+    @Autowired
+    private MemberRepository memberRepository;
+
+    private Member savedMember;
+
+    @BeforeEach
+    void beforeEach() {
+        createMember();
+    }
+
+    @Test
+    @DisplayName("loginId로 회원 조회")
+    void findByLoginId() {
+        //given
+
+        //when
+        Member findMember = memberValidator.findByLoginId(savedMember.getLoginId());
+
+        //then
+        assertThat(findMember.getId()).isEqualTo(savedMember.getId());
+    }
+
+    @Test
+    @DisplayName("loginId로 회원 조회#예외")
+    void findByLoginIdException() {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> memberValidator.findByLoginId("noLoginId"))
+                .isInstanceOf(NoSuchElementException.class);
+    }
+
+    private void createMember() {
+        Address address = Address.builder().mainAddress("mainAddress").detailAddress("detailAddress").build();
+        Member member = Member.builder()
+                .loginId("harry")
+                .loginPw("abcd1234!")
+                .name("harrypotter")
+                .tel("077-1234-1234")
+                .address(address)
+                .identity(STUDENT)
+                .grade(QUAFFLE)
+                .roles(Collections.singletonList("MEMBER"))
+                .build();
+        savedMember = memberRepository.save(member);
+    }
+
+}
