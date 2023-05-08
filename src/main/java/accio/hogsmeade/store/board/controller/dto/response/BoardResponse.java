833a7c66399e80f0db3d51064f9812c5776abcc8
leeyr0412 <leeyr0412@naver.com>
2023-05-09 00:13:05 +0900

    🧙 feat: 게시판 목록 조회 Repository 구현

--- src/main/java/accio/hogsmeade/store/board/controller/dto/response/BoardResponse.java
+++ src/main/java/accio/hogsmeade/store/board/controller/dto/response/BoardResponse.java
@@ -1,8 +1,12 @@
 package accio.hogsmeade.store.board.controller.dto.response;
 
+import accio.hogsmeade.store.common.model.UploadFile;
 import accio.hogsmeade.store.member.model.Identity;
 import lombok.Builder;
 import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Data
 public class BoardResponse {
@@ -15,13 +19,13 @@ public class BoardResponse {
     private boolean file;
 
     @Builder
-    public BoardResponse(Long boardId, String category, String title, int hit, Identity identity, String createdDate, boolean file) {
+    public BoardResponse(Long boardId, String category, String title, int hit, Identity identity, LocalDateTime createdDate, UploadFile file) {
         this.boardId = boardId;
         this.category = category;
         this.title = title;
         this.hit = hit;
         this.identity = identity;
-        this.createdDate = createdDate;
-        this.file = file;
+        this.createdDate = createdDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        this.file = file != null;
     }
 }
