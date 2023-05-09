71f1dff3b24a9ba3fe78dd4a0901cb49b1341425
leeyr0412 <leeyr0412@naver.com>
2023-05-09 23:01:25 +0900

    🧙 feat: 게시판 목록 조회 서비스 구현

--- src/main/java/accio/hogsmeade/store/board/model/service/BoardService.java
+++ src/main/java/accio/hogsmeade/store/board/model/service/BoardService.java
@@ -1,7 +1,11 @@
 package accio.hogsmeade.store.board.model.service;
 
+import accio.hogsmeade.store.board.controller.dto.response.BoardResponse;
+import accio.hogsmeade.store.board.model.repository.dto.BoardSearchCondition;
 import accio.hogsmeade.store.board.model.service.dto.AddBoardDto;
 import accio.hogsmeade.store.board.model.service.dto.EditBoardDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
@@ -9,4 +13,6 @@ public interface BoardService {
     Long writeBoard(String loginId, Long categoryId, AddBoardDto addBoardDto);
 
     Long editBoard(String loginId, Long boardId, EditBoardDto editBoardDto);
+
+    Page<BoardResponse> getBoardList(BoardSearchCondition condition, Pageable pageable);
 }
